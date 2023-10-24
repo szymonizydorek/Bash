5ea24500045c86cdb51072d15a73e9bc81c78c45
szymonizydorek <izydorek.szymon@gmail.com>
2023-10-24 21:01:32 +0200

    1st Ping script typo change

--- day1/1_Ping.sh
+++ day1/1_Ping.sh
@@ -1,3 +1,3 @@
-echo -e "This is day 2 of Bash Challenge \n Provide website address: "
+echo -e "This is day 1 of Bash Challenge \n Provide website address: "
 read website
 ping -c 3 $website | grep loss
