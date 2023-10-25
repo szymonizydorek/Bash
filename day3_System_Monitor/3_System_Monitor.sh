5593f5069bff3e335132c9a0c00e71287f8a880e
szymonizydorek <izydorek.szymon@gmail.com>
2023-10-25 20:42:58 +0200

    3rd day - fixing System Monitor

--- day3_System_Monitor/3_System_Monitor.sh
+++ day3_System_Monitor/3_System_Monitor.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+
 # Function to get CPU usage
 function 1_cpu_usage {
   top -n 1 | head -n 8 | awk '/CPU usage/ {print $1, $2, $3}'
 }
 
 
-# Funcion to get memoty usage
-function 2_memory {
-  free
+function 2_disk_usage {
+  x=$(df -h | grep /$ | awk '{print $5}')
+  echo 'Disk usage:' $x
 }
-echo 'The displaying system information below:' 
+
+echo 'The displaying system information: ' 
 1_cpu_usage
+2_disk_usage
+
